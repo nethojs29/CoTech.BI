8a0ca4b948a27a2c7a5686519b0a0004ff9f07eb
Ernesto Salazar <nethojs29@gmail.com>
2017-09-12 18:24:46 -0700

    Modelos Gastos

---
+++ CoTech.Bi/Modules/Expenses/Models/ExpenseGroupEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using CoTech.Bi.Core.Companies.Models;
+
+namespace CoTech.Bi.Modules.Expenses.Models{
+    public class ExpenseGroupEntity{
+        public long Id{ set; get; }
+        [Required]
+        public string Name{ set; get; }
+        [Required]
+        public int TypeId{ set; get; }
+        public ExpenseTypeEntity Type{ set; get; }
+        [Required]
+        public int CompanyId{ set; get; }
+        public CompanyEntity Company{ set; get; }
+        
+        public DateTime CreatedAt{ set; get; }
+        public DateTime? DeletedAt{ set; get; }
+    }
+}
