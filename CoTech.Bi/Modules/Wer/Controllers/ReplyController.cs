7c56d142eadccb0f383bc3cf258413989ea6829a
EnriqueMoya <enrique.moya08@gmail.com>
2017-11-02 14:14:08 -0700

    remove substring message notification

--- CoTech.Bi/Modules/Wer/Controllers/ReplyController.cs
+++ CoTech.Bi/Modules/Wer/Controllers/ReplyController.cs
@@ -118,7 +118,7 @@ namespace CoTech.Bi.Modules.Wer.Controllers
                 var response = new MessageResponse(values);
                 string messageString = values.User.Name + " " + values.User.Lastname + ": " + response.Message; 
                 var userNotify = new List<long>(){idUser,creator};
-                _notifications.SendNotification(userNotify,messageString.Substring(0,140),response);
+                _notifications.SendNotification(userNotify,messageString,response);
                 return new ObjectResult(response){StatusCode = 201};
             }
             catch (Exception e)
